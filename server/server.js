1e05e5b16f59148f85e7226c50b4f3d43c8b2887
suraj <surajkiranpanda98@gmail.com>
2025-06-15 00:23:53 +0530

    commited

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -21,5 +25,9 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('server is running'));
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log(`server is listening at port${port}`));
